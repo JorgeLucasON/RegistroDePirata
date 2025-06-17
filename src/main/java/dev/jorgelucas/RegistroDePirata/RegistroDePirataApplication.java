212879220e41abe86c94e4d9b7f3e59b05c6185c
JorgeLucasON <jorgelucasolivx@gmail.com>
2025-06-17 11:20:28 -0300

    first commit

---
+++ src/main/java/dev/jorgelucas/RegistroDePirata/RegistroDePirataApplication.java
@@ -0,0 +1,13 @@
+package dev.jorgelucas.RegistroDePirata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDePirataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDePirataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jorgelucas/RegistroDePirata/RegistroDePirataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jorgelucas.RegistroDePirata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDePirataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
