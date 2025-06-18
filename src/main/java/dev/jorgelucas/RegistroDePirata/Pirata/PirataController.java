b3913b810ea080f56d6ecc09116ee0de1f7b21e7
JorgeLucasON <jorgelucasolivx@gmail.com>
2025-06-18 11:29:58 -0300

    feat: Altera  a rota do controller para listar os ninjas e injeta a dependencia do SERVICE e do REPOSITORY

--- src/main/java/dev/jorgelucas/RegistroDePirata/Pirata/PirataController.java
+++ src/main/java/dev/jorgelucas/RegistroDePirata/Pirata/PirataController.java
@@ -1,4 +1,53 @@
 package dev.jorgelucas.RegistroDePirata.Pirata;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(name = "piratas")
 public class PirataController {
+
+    private PirataService pirataService;
+
+    public PirataController(PirataService pirataService) {
+        this.pirataService = pirataService;
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasvindas(){
+        return "Essa e minha primeira mensagem nessa rota";
+    }
+
+    // Adicionar Pirata (CREATE)
+    @GetMapping("/criar")
+    public String criarPirata(){
+       return "Ninja criado";
+    }
+
+    // Mostra todos os Piratas (READ)
+    @GetMapping("/listar")
+    public List<PirataModel> listarPirata(){
+        return pirataService.listarPirata();
+    }
+
+    // Mostra Piratas por Id (READ)
+    @GetMapping("/listar/{id}")
+    public String listarPirataPorId(){
+        return "Pirata por ID";
+    }
+
+    // Alterar dados dos Piratas(UPDATE)
+    @PutMapping("/alterar/{id}")
+    public String atualizarPirata(){
+        return "Altera Pirata por id ";
+    }
+
+    // Deletar Pirata (Delate)
+    @DeleteMapping("/deletar/{id}")
+    public String deletarPirataPorID(){
+        return "Ninja deletado";
+    }
+
 }
--- src/main/java/dev/jorgelucas/RegistroDePirata/Pirata/PirataService.java
+++ src/main/java/dev/jorgelucas/RegistroDePirata/Pirata/PirataService.java
@@ -1,4 +1,54 @@
 package dev.jorgelucas.RegistroDePirata.Pirata;
 
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PirataService {
+
+    private PirataRepository pirataRepository;
+
+    public PirataService(PirataRepository pirataRepository) {
+        this.pirataRepository = pirataRepository;
+    }
+
+    // listar todos os meus Piratas
+    public List<PirataModel>listarPirata(){
+        return pirataRepository.findAll();
+    }
+
+    // listar piratas por id
+    public PirataModel listarPirataPorId(Long id){
+        Optional<PirataModel> pirataPorId = pirataRepository.findById(id);
+        return pirataPorId.orElse(null);
+    }
+
+    // Criar um novo Pirata
+    public PirataModel criarPirata(PirataModel pirata){
+        return pirataRepository.save(pirata);
+    }
+
+    // Deletar Pirata
+    public void deletarPirataPorID(Long id){
+        pirataRepository.deleteById(id);
+    }
+
+    // Atualizar Pirta
+    public PirataModel altualizarPirata(Long id, PirataModel pirataAtualizado){
+        if(pirataRepository.existsById(id)) {
+            pirataAtualizado.setId(id);
+            return pirataRepository.save(pirataAtualizado);
+        }
+        return null;
+    }
+
+
+
+
+
+
+
 }
